7d257f80ea0c59617ee3ac48ff8c5c95f10c079d
Manuel de la Peña <manuel.delapenya@liferay.com>
2013-05-22 14:47:18 +0200

    Support one parameter when executing the script

--- scripts/deploy/package-plugins.sh
+++ scripts/deploy/package-plugins.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-TYPE_OF_PLUGINS="themes webs hooks layouttpl portlets"
+TYPE_OF_PLUGINS="themes webs hooks layouttpl portlets shared"
 
 for plugin in $TYPE_OF_PLUGINS
 do
-        find ${WORKSPACE}/$plugin/* -maxdepth 0 -type d -exec sh -c "cd {}; /opt/ant/apache-ant-1.8.3/bin/ant war" \;
+        find ${WORKSPACE}/$plugin/* -maxdepth 0 -type d -exec sh -c "cd {}; /usr/bin/ant $1" \;
 done
