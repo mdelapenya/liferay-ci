a47cd3f1cdf612f32278b09e9a1898961c2f8ee0
Manuel de la Peña <manuel.delapenya@liferay.com>
2013-09-24 11:04:36 +0200

    Use default postgres user to execute commands

--- scripts/database/postgresql/dropDatabase.sh
+++ scripts/database/postgresql/dropDatabase.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 MHOST="$1"
 MUSER="$2"
-MPASS="$3"
-MDB="$4"
- 
+MDB="$3"
+
 # Detect the paths for the PostgreSQL command
 POSTGRESQL=$(which psql)
- 
-if [ $# -ne 4 ]
+
+if [ $# -ne 3 ]
 then
-	echo "Usage: $0 {Hostname}{PostgreSQL-User-Name}{PostgreSQL-Password}{PostgreSQL-Database-Name}"
-	echo "Deletes and (re) create the indicated database"
-	exit 1
+        echo "Usage: $0 {Database hostname}{PostgreSQL-User}{PostgreSQL-Database-Name}"
+        echo "Deletes and (re) create the indicated database"
+        exit 1
 fi
 
 echo "Deleting database $MDB ..."
-$POSTGRESQL -h ${MHOST} -U $MUSER -c "DROP DATABASE $MDB"
+$POSTGRESQL -h $MHOST -U postgres -c "DROP DATABASE ${MDB};"
 echo "Creating the database $MDB . . ."
-$POSTGRESQL -h ${MHOST} -U $MUSER -c "CREATE DATABASE ${MDB} ENCODING = 'UTF8'"
+$POSTGRESQL -h $MHOST -U postgres -c "CREATE DATABASE ${MDB} WITH OWNER ${MUSER} ENCODING 'UTF8';"
 echo "Database $MDB has been created"
