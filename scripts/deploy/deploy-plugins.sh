fa78cccc7c18d82bd841c46d3069e81c3d0762f9
Manuel de la Peña <manuel.delapenya@liferay.com>
2014-01-24 09:37:35 +0100

    Support deploying JAR files, as OSGi plugins are not war files

--- scripts/deploy/deploy-plugins.sh
+++ scripts/deploy/deploy-plugins.sh
@@ -16,7 +16,7 @@ do
 	ssh liferay@${server} "find /deploy/${environment}/webapps -type d -name *${plugin}* -maxdepth 1 -exec rm -fr {} \;"
 
   echo Deploying the $plugin plugins
-  find ${LIFERAY_PLUGINS_REPOSITORY} -name $plugin*.war -exec scp {} ${server}:/home/liferay/${environment}/deploy \;
+  find ${LIFERAY_PLUGINS_REPOSITORY} -name $plugin*.?ar -exec scp {} ${server}:/home/liferay/${environment}/deploy \;
 done
 
 echo The plugins "$plugins" has been copied to the autodeploy folder present at /home/liferay/${environment}/deploy. The autodeploy process will start in a few seconds
